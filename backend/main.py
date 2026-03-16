56bc94630d86fe649c002d8d3b0702f72f510633
AtharvaParadkar <atharvaparadkar91@gmail.com>
2026-03-16 17:20:26 +0530

    Implement initial FastAPI setup with SQLAlchemy for user signup and database schema definition.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, TEXT, VARCHAR, LargeBinary
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app = FastAPI()
+
+DATABASE_URL = "postgresql://postgres:qwerty@localhost:5432/spotify_clone"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+db = SessionLocal()
 
 class CreateUser(BaseModel):
     username: str
     email: str
     password: str
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(TEXT, primary_key=True)
+    name = Column(VARCHAR(100))
+    email = Column(VARCHAR(100))
+    password = Column(LargeBinary)
 
 @app.post('/signup')
 def signup_user(user: CreateUser):
@@ -16,3 +36,5 @@ def signup_user(user: CreateUser):
     #add user to db
     
     return "😀"
+
+Base.metadata.create_all(bind=engine)
