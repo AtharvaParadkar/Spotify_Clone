edefa37620a46f9afc3309d569818bbfaa17e9ea
AtharvaParadkar <atharvaparadkar91@gmail.com>
2026-06-17 16:45:56 +0530

    Add song model and update upload endpoint to save song details in the database

--- backend/routes/song.py
+++ backend/routes/song.py
@@ -6,6 +6,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import APIRouter, UploadFile, File, Form, Depends 
 from sqlalchemy.orm import Session
+from models.song import Song
 from middleware.auth_middleware import auth_middleware
 from database import get_db
 
@@ -21,7 +22,7 @@ cloudinary.config(
     secure=True
 )
 
-@router.post('/upload')
+@router.post('/upload', status_code = 201)
 def upload_song(
     song: UploadFile = File(...), 
     thumbnail: UploadFile = File(...), 
@@ -36,4 +37,17 @@ def upload_song(
     print(song_res)
     thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type = 'image', folder = f'songs/{song_id}')
     print(thumbnail_res)
-    return 'ok'
+    
+    new_song = Song(
+        id = song_id,
+        song_name = song_name,
+        artist = artist,
+        hex_code = hex_code,
+        song_url = song_res['url'],
+        thumbnail_url = thumbnail_res['url']
+    )
+    
+    db.add(new_song)
+    db.commit()
+    db.refresh(new_song)
+    return new_song
