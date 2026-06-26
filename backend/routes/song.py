12868c6dad171b02245072d1a8cd3ea186b95df3
AtharvaParadkar <atharvaparadkar91@gmail.com>
2026-06-26 14:41:46 +0530

    Add favorite song functionality with model and endpoint

--- backend/routes/song.py
+++ backend/routes/song.py
@@ -1,14 +1,16 @@
-from uuid import uuid4
+import uuid
 
 import cloudinary
 import cloudinary.uploader
 import os
 from dotenv import load_dotenv
 from fastapi import APIRouter, UploadFile, File, Form, Depends 
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from models.song import Song
 from middleware.auth_middleware import auth_middleware
 from database import get_db
+from pydantic_scheme.favorite_song import FavoriteSong
+from models.favorite import Favorite
 
 load_dotenv()
 
@@ -56,3 +58,19 @@ def upload_song(
 def list_songs(db: Session = Depends(get_db), auth_details = Depends(auth_middleware)):
     songs = db.query(Song).all()
     return songs
+
+@router.post('/favorite')
+def favorite_song(song: FavoriteSong,  db: Session = Depends(get_db), auth_details = Depends(auth_middleware)):
+   user_id = auth_details['uid']
+   
+   fav_song = db.query(Favorite).filter(Favorite.song_id == song.song_id, Favorite.user_id == user_id).first()
+   
+   if fav_song:
+       db.delete(fav_song)
+       db.commit()
+       return {'message': False}
+   else:
+       new_fav = Favorite(id = str(uuid.uuid4()), song_id = song.song_id, user_id = user_id)
+       db.add(new_fav)
+       db.commit()
+       return {'message': True}
